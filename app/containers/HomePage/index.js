f1cd6481b3e6e7ec43ffbedd651fa60d174ba318
Anthonie Lance <alance33@gmail.com>
2017-06-08 11:39:10 -0400

    Loading data from server with redux-saga

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,7 +10,7 @@
  */
 
 import React from 'react';
-import NavigationContainer from '../../containers/NavigationContainer';
+import NavigationContainer from '../NavigationContainer';
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 

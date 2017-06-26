93f1ad230682e40b9bd73fd94b6e5af3bc83f730
Anthonie Lance <alance33@gmail.com>
2017-06-26 15:05:11 -0400

    Tackling a realistically complex feature with your new skills

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -11,12 +11,46 @@ import TextInput from '../TextInput';
 
 
 class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  state = {};
-
   static propTypes = {
-    login: React.PropTypes.func.isRequired,
-    cancelLogin: React.PropTypes.func.isRequired,
+    addLink: React.PropTypes.func.isRequired,
+    addLinkCancelled: React.PropTypes.func.isRequired,
+    topicName: React.PropTypes.string.isRequired,
   };
+
+  state = {
+    urlError: '',
+    descriptionError: '',
+  };
+
+  onAdd = () => {
+    const url = this.url.value();
+    const description = this.description.value();
+    let urlError = null;
+    let descriptionError = null;
+
+    if (!url.match(/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,256}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/)) {
+      urlError = 'Please provide a valid URL';
+    }
+
+    if (!description) {
+      descriptionError = 'Please provide a valid description';
+    }
+
+    this.setState({
+      urlError,
+      descriptionError,
+    });
+
+    if (urlError || descriptionError) {
+      return;
+    }
+
+    this.props.addLink({
+      topicName: this.props.topicName,
+      url,
+      description,
+    });
+  }
 
   render() {
     return (
@@ -28,16 +62,20 @@ class LinkForm extends React.Component { // eslint-disable-line react/prefer-sta
           <TextInput
             placeholder="URL"
             className={styles.input}
+            errorText={this.state.urlError}
+            ref={(f) => (this.url = f)}
           />
 
           <TextInput
             placeholder="Description"
             className={styles.input}
+            errorText={this.state.descriptionError}
+            ref={(f) => (this.description = f)}
           />
 
           <div className={styles.actionContainer}>
-            <div className={styles.button} onClick={this.props.cancelLogin}>cancel</div>
-            <div className={styles.button} onClick={this.login}>log in</div>
+            <div className={styles.button} onClick={this.props.addLinkCancelled}>cancel</div>
+            <div className={styles.button} onClick={this.onAdd}>add</div>
           </div>
         </div>
       </div>
